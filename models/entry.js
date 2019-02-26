7c1bf73bca11bbaf436fc7f2d7d453159c373a07
kayday <kadaniilo@gmail.com>
2019-02-26 11:31:19 +0300

    seq -> mongoose

--- models/entry.js
+++ models/entry.js
@@ -1,16 +1,1 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const entry = sequelize.define('entry', {
-    title: DataTypes.STRING,
-    body: DataTypes.TEXT
-  }, {});
-
-  // return 5 last inserted entries
-  entry.mostRecent = async () => {
-      return await entry.findAll({
-          limit: 5,
-          order: [['createdAt', 'DESC']]
-      });
-  };
-  return entry;
-};
+// Your code here
--- routes/entries.js
+++ routes/entries.js
@@ -1,53 +1,51 @@
-var express = require('express');
-var router = express.Router();
-const models = require('./../models/index')
-const Entry = models.entry;
+const express = require('express');
+const router = express.Router();
+const Entry = require('../models/entry');
 console.log('started entries');
+
+
 // entries
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
     let entries = await Entry.mostRecent();
     console.log(entries);
     res.render('entries/index', { entries });
 });
 
-router.post('/', async function(req, res, next) {
-    newEntry = await Entry.create({title: req.body.title, body: req.body.body});
+router.post('/', async function (req, res, next) {
+    newEntry = new Entry({ title: req.body.title, body: req.body.body });
     res.redirect(`/entries/${newEntry.id}`);
 });
 
 //new entries
-router.get('/new', function(req, res, next) {
+router.get('/new', function (req, res, next) {
     res.render('entries/new');
 });
 
 //detail entry
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', async function (req, res, next) {
     let entry = await Entry.findById(req.params.id);
-    res.render('entries/show', {entry});
+    res.render('entries/show', { entry });
 });
 
-router.put('/:id', async function(req, res, next) {
+router.put('/:id', async function (req, res, next) {
     let entry = await Entry.findById(req.params.id);
+ 
+    entry.title = req.body.title;
+    entry.body = req.body.body;
+    await entry.save();
 
-    entry.update({
-        title: req.body.title,
-        body: req.body.body
-    }).then(() => {
-        console.log(entry.id);
-        res.redirect(`/entries/${entry.id}`);
-    });
+    res.redirect(`/entries/${entry.id}`);
 
 });
 
-router.delete('/:id', async function(req, res, next) {
-    let entry = await Entry.findById(req.params.id);
-    entry.destroy();
+router.delete('/:id', async function (req, res, next) {
+    await Entry.deleteOne({'_id': req.params.id});
     res.redirect('/');
 });
 
-router.get('/:id/edit', async function(req, res, next) {
+router.get('/:id/edit', async function (req, res, next) {
     let entry = await Entry.findById(req.params.id);
-    res.render('entries/edit', {entry});
+    res.render('entries/edit', { entry });
 });
 module.exports = router;
 
